4eebe5a921e66ded1679ff7f2dcc557de58db379
keshavcharan <keshavcharan@gmail.com>
2016-03-13 18:50:44 -0400

    Added bottom Tabs

--- app/src/main/java/taskman/handyapps/com/taskman/TaskManagerActivity.java
+++ app/src/main/java/taskman/handyapps/com/taskman/TaskManagerActivity.java
@@ -4,10 +4,12 @@ import android.app.ActionBar;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
+import android.support.design.widget.TabLayout;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TableLayout;
 
-public class TaskManagerActivity extends FragmentActivity
+public class TaskManagerActivity extends AppCompatActivity
 {
     TasksPagerAdapter tasksPagerAdapter;
     @Override
@@ -15,11 +17,14 @@ public class TaskManagerActivity extends FragmentActivity
     {
         super.onCreate(savedInstanceState);
 
-
-
         setContentView(R.layout.activity_task_manager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
         ViewPager viewPager = (ViewPager) findViewById(R.id.tabspager);
         tasksPagerAdapter = new TasksPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(tasksPagerAdapter);
+
     }
 }
